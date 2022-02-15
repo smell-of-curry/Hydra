f0b201aa133d8f2af7e8a9bdeb4ece085c6b0596
Smell of curry <75345244+smell-of-curry@users.noreply.github.com>
2022-02-14 22:28:18 -0500

    way better
    remove the enchantment detector

--- scripts/modules/item_detection.js
+++ scripts/modules/item_detection.js
@@ -1,61 +1,60 @@
-import { world } from "mojang-minecraft"
+import {
+  ItemStack,
+  Items,
+  world,
+  MinecraftEnchantmentTypes,
+} from "mojang-minecraft";
 
-const items = [
-    'minecraft:portal',
-    'minecraft:item.soul_campfire',
-    'minecraft:fire',
-    'minecraft:item.chain',
-    'minecraft:item.warped_door',
-    'minecraft:item.crimson_door',
-    'minecraft:item.campfire',
-    'minecraft:item.kelp',
-    'minecraft:bee_nest',
-    'minecraft:beehive',
-    'minecraft:item.wooden_door',
-    'minecraft:item.iron_door',
-    'minecraft:item.cake',
-    'minecraft:item.reeds',
-    'minecraft:item.camera',
-    'minecraft:item.frame',
-    'minecraft:pistonarmcollision',
-    'minecraft:movingBlock',
-    'minecraft:item.cake',
-    'minecraft:item.flower_pot',
-    'minecraft:info_update2',
-    'minecraft:stickyPistonArmCollision',
-    'minecraft:movingblock',
-    'minecraft:invisiblebedrock',
-    'minecraft:glowingobsidian',
-    'minecraft:flowing_water',
-    'minecraft:flowing_lava',
-    'minecraft:item.nether_sprouts'
-]
+const banned_items = [
+  "minecraft:portal",
+  "minecraft:item.soul_campfire",
+  "minecraft:fire",
+  "minecraft:item.chain",
+  "minecraft:item.warped_door",
+  "minecraft:item.crimson_door",
+  "minecraft:item.campfire",
+  "minecraft:item.kelp",
+  "minecraft:bee_nest",
+  "minecraft:beehive",
+  "minecraft:item.wooden_door",
+  "minecraft:item.iron_door",
+  "minecraft:item.cake",
+  "minecraft:item.reeds",
+  "minecraft:item.camera",
+  "minecraft:item.frame",
+  "minecraft:pistonarmcollision",
+  "minecraft:movingBlock",
+  "minecraft:item.cake",
+  "minecraft:item.flower_pot",
+  "minecraft:info_update2",
+  "minecraft:stickyPistonArmCollision",
+  "minecraft:movingblock",
+  "minecraft:invisiblebedrock",
+  "minecraft:glowingobsidian",
+  "minecraft:flowing_water",
+  "minecraft:flowing_lava",
+  "minecraft:item.nether_sprouts",
+];
 
-const ItemChecker = () => {
-    world.events.tick.subscribe(() => {
-        var players = world.getPlayers()
-        for (const player of players) {
-            let inv = player.getComponent('minecraft:inventory').container
-            let inv_items = []
-            for (let i = 0; i < inv.size; i++) {
-                try {
-                    let inv_i = inv.getItem(i)
-                    inv_items.push(inv_i.id)
-                } catch (e) { }
-            }
-            inv_items.forEach(item => {
-                if (items.includes(item)) {
-                    try { world.getDimension("overworld").runCommand(`clear "${player.nameTag.replace(/"/g, '') || player.name.replace(/"/g, '')}"`, world.getDimension('overworld')) } catch (e) { }
-                    player.triggerEvent('hydra:kick')
-                }
-            })
-            items.forEach(item => {
-                try { world.getDimension("overworld").runCommand(`clear @a ${item.split(':')[1]}`, world.getDimension('overworld')) } catch (e) { }
-            })
+const allEnchantments = Object.values(MinecraftEnchantmentTypes);
+
+export function ItemChecker() {
+  world.events.tick.subscribe(() => {
+    for (const player of world.getPlayers()) {
+      let inventory = player.getComponent("minecraft:inventory").container;
+      for (let i = 0; i < inventory.size; i++) {
+        let item = inventory.getItem(i);
+        if (banned_items.includes(item.id)) {
+          inventory.setItem(i, new ItemStack(Items.get("minecraft:air"), 0, 0));
         }
-    })
+        const itemEnchantments = item.getComponent("enchantments").enchantments;
+        for (let ench of allEnchantments) {
+          let enchLvl = itemEnchantments.hasEnchantment(ench);
+          if (enchLvl > ench.maxLevel) {
+            itemEnchantments.removeEnchantment(ench);
+          }
+        }
+      }
+    }
+  });
 }
-
-// mind making this cool someone please anyone?
-
-export { ItemChecker }
